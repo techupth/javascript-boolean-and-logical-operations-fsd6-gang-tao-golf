8bb0797558d37870f63f1d44ae3da0dd32654a36
Prechak <benedict.precha@gmail.com>
2024-05-09 16:00:16 +0700

    (ex-1): refractor code2

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
 // James
-let isOver18 = false;
+let isOver18 = true;
 let hasCriminalBlacklist = false;
 
-let isAllow = isOver18 && hasCriminalBlacklist;
+let isAllow = isOver18 && !hasCriminalBlacklist;
 
 // Start coding here
 
